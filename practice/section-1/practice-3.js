7389cd7b872d011bae8de6cfe5844d6e5d883348
MURPHY LI (ITA-ISDC-ISD-OOCLL/ZHA) <LIMU2@OOCL.COM>
2019-07-08 11:48:21 +0800

    commit

--- practice/section-1/practice-3.js
+++ practice/section-1/practice-3.js
@@ -1,5 +1,23 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var r = new Array();
+  // for (var i=0;i<collectionA.length;i++) {
+  //   for (var item in objectB) {
+  //     if (collectionA[i] == item) {
+  //       r.push(collectionA[i]);
+  //       break;
+  //     }
+  //   }
+  // }
+  var collectionB=objectB.value;
+  for (var i = 0; i < collectionA.length; i++) {
+    for (var j = 0; j < collectionB.length; j++) {
+      if (collectionA[i] == collectionB[j]) {
+        r.push(collectionB[j]);
+        break;
+      }
+    }
+  }
+  return r;
 }
